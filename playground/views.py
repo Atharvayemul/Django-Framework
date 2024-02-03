17a6a27519e6efb2ae7ca1c3451c00b838650d7f
Atharva Yemul <atharvayemul04@gmail.com>
2024-02-03 20:05:55 +0530

    First commit

---
+++ playground/views.py
@@ -0,0 +1,16 @@
+from django import views
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def calculate ():
+    x = 1
+    y = 2
+    return x
+
+def say_hello(request):
+    x = 1
+    y = 2
+    return render(request,'hello.html',{'name':'Atharva'})
+
+
