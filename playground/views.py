9b62da67ba4ba52fc64b12b05d7636640ad27f2b
Atharva Yemul <atharvayemul04@gmail.com>
2024-02-06 16:33:18 +0530

    Queryset

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,21 @@
 from django import views
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from store.models import  Product
+from store.models import Collection
 
 # Create your views here.
 
 
 
 def say_hello(request):
-    query_set =  Product.objects.all()
-   
     
-    return render(request,'hello.html',{'name':'Atharva'})
+    #Products : inventory < 10 and price < 20
+    
+    query_set = Product.objects.filter(inventory__lt = 10 ,unit_price__lt = 20)
+  
+    
+    return render(request,'hello.html',{'name':'Atharva','products':list(query_set)})
 
 
