9cecba5cf658d84ea557e74aed16581299ab490e
Atharva Yemul <atharvayemul04@gmail.com>
2024-02-06 16:08:24 +0530

    Updated

--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,16 @@
 from django import views
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import  Product
+
 # Create your views here.
 
-def calculate ():
-    x = 1
-    y = 2
-    return x
+
 
 def say_hello(request):
-    x = calculate()
+    query_set =  Product.objects.all()
+   
+    
     return render(request,'hello.html',{'name':'Atharva'})
 
 
