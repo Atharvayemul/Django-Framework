faba5efa197cc3181c95f31274c45f4b74922924
Atharva Yemul <atharvayemul04@gmail.com>
2024-02-11 19:26:36 +0530

    All Method

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,6 @@
 from django import views
 from django.shortcuts import render
-from django.db.models import Q
+from django.db.models import Q,F
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from store.models import  Product
@@ -14,8 +14,9 @@ def say_hello(request):
     
     #Products : inventory < 10 and price < 20
     
-    query_set = Product.objects.filter(Q(inventory__lt = 10) | Q(unit_price__lt=20))
-  
+    # query_set = Product.objects.filter(inventory=F('unit_price'))
+    
+    query_set = Product.objects.all()[:5]
     
     return render(request,'hello.html',{'name':'Atharva','products':list(query_set)})
 
